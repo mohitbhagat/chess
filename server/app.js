07149d50ed020866deedde73cc0005ad07e4dcda
Mohit Bhagat <m5bhagat@edu.uwaterloo.ca>
2020-05-03 11:51:05 -0400

    Created first production build

---
+++ server/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const Game = require('./game.js');
+const path = require('path');
+const socketIo = require('socket.io');
+
+const port = process.env.PORT || 8000;
+
+let app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => { res.sendfile(path.join(__dirname, 'build', 'index.html'));});
+
+let server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log('listening on ' + port);
+});
+
+let io = socketIo(server);
+
+io.on("connection", socket => {
+    console.log("New client connected");
+    let game = new Game.Game(socket);
+    socket.on('init', () => {
+        game.initGame();
+    })
+    socket.on('move', data => {
+        game.makeMove(data.startingCoordinate, data.endingCoordinate);
+    });
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+    });
+});
+
+module.exports = app;
---
+++ server/build/precache-manifest.eec22d4345fcc78eacedd4b4294c7c3f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ead0d908c9294a0e748b54d40850498",
+    "url": "/index.html"
+  },
+  {
+    "revision": "795e263327b156b3fcce",
+    "url": "/static/css/main.31442b03.chunk.css"
+  },
+  {
+    "revision": "fb19b9337a9a676ae5d4",
+    "url": "/static/js/2.afef7880.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.afef7880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "795e263327b156b3fcce",
+    "url": "/static/js/main.bccdf1ca.chunk.js"
+  },
+  {
+    "revision": "e8c9090cdd1818d9afa5",
+    "url": "/static/js/runtime-main.d8484e8d.js"
+  },
+  {
+    "revision": "f70e51a38ece394e3ff5fd4a21ca7461",
+    "url": "/static/media/Bb.f70e51a3.png"
+  },
+  {
+    "revision": "7c4e4775996cf89a84a2892e090f35f4",
+    "url": "/static/media/Kb.7c4e4775.png"
+  },
+  {
+    "revision": "d42dad2343ee56b34371c7e1dfc166b7",
+    "url": "/static/media/Nb.d42dad23.png"
+  },
+  {
+    "revision": "fbe7f40a88e7ec7938eca998581ac3f7",
+    "url": "/static/media/Pb.fbe7f40a.png"
+  },
+  {
+    "revision": "0a56b24f791a85687cea8793a907c42e",
+    "url": "/static/media/Qb.0a56b24f.png"
+  },
+  {
+    "revision": "0592febe80a4e738edd17ab948faaa7a",
+    "url": "/static/media/Rb.0592febe.png"
+  }
+]);
